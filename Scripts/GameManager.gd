95c114ab5f661c47beecbf45d222bb15dab1a956
pondus314 <peter.ondus@gmail.com>
2025-01-25 23:01:24 +0100

    make the menu menu

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -1,10 +1,20 @@
 class_name GameManager extends Control
 
+enum GameState{
+	MAIN_MENU, OPTIONS_MENU, PAUSE_MENU, GAME
+}
+
+
 @export var level_count = 2
 
 @onready var hud = $HUD
 @onready var gui = $GUI
 @onready var main_scene = $Game
+@onready var stamina_bar = $"HUD/Bubble Girl UI/TextureProgressBar"
+@onready var main_menu = $GUI/MainMenu
+@onready var pause_menu = $GUI/PauseMenu
+@onready var options_menu = $GUI/OptionsMenu
+
 
 var level_instance: Node2D
 var game_paused = true
@@ -12,17 +22,21 @@ var curr_level
 var retry_text = null
 var level_menu_loaded = false
 var name_tween
-
+var current_state = GameState.MAIN_MENU
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	get_tree().paused = true
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	Global.main_scene = self
-	start_game()
+	main_menu.show()
+	options_menu.hide()
+	pause_menu.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("exit"): 
+		on_escape_pressed()
 
 func unload_level() -> void:
 	
@@ -62,23 +76,32 @@ func next_level():
 		#load_menu(MenuType.MAIN)
 
 func start_game():
-	#load_menu(MenuType.PAUSE)
-	load_level("level_2")
+	current_state = GameState.GAME
+	load_level("level_1")
 	unpause_game()
 
 func pause_game() -> void:
+	current_state = GameState.PAUSE_MENU
+
 	get_tree().paused = true
 	game_paused = true
-
+	main_scene.hide()
 	hud.hide()
+	main_menu.hide()
+	pause_menu.show()
 	gui.show()
+	
+	
 
 func unpause_game() -> void:
+	print("beeee")
 	get_tree().paused = false
 	game_paused = false
 	main_scene.show()
-	#hud.show()
+	hud.show()
 	gui.hide()
+	current_state = GameState.GAME
+	print("unpausing game")
 
 func quit_game() -> void:
 	get_tree().quit()
@@ -86,3 +109,38 @@ func quit_game() -> void:
 
 func game_over(death_text = "You died, I think?") -> void:
 	reload_level(false)
+
+
+func on_play_button_pressed() -> void:
+	start_game()
+
+func on_resume_button_pressed() -> void:
+	unpause_game()
+
+func open_options() -> void:
+	main_menu.hide()
+	options_menu.show()
+	current_state = GameState.OPTIONS_MENU
+
+func exit_options() -> void:
+	options_menu.hide()
+	main_menu.show()
+	current_state = GameState.MAIN_MENU
+
+
+func back_to_main_menu() -> void:
+	current_state = GameState.MAIN_MENU
+	pause_menu.hide()
+	unload_level()
+	main_menu.show()
+
+func on_escape_pressed() -> void:
+	if current_state == GameState.MAIN_MENU:
+		quit_game()
+	elif current_state == GameState.OPTIONS_MENU:
+		exit_options()
+	elif current_state == GameState.PAUSE_MENU:
+		unpause_game()
+	elif current_state == GameState.GAME:
+		pause_game()
+	
