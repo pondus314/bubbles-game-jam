3ffc87b5b221592ab3471ebfc43c18cea584d326
pondus314 <peter.ondus@gmail.com>
2025-01-25 15:06:34 +0100

    implement floating as separate

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,7 +6,7 @@ enum JumpState {FALLING, JUMPING, FLOATING}
 @export var health = 1
 @export var jump_power = 550
 @export var coyote_time = 0.2
-@export var jump_to_float_v = -50
+@export var jump_to_fall_v = 100
 @export var max_float_time = 1.5
 @export var has_float = false
 @export var gravity_dict = {
@@ -14,13 +14,15 @@ enum JumpState {FALLING, JUMPING, FLOATING}
 	JumpState.JUMPING: 500,
 	JumpState.FLOATING: -400,
 }
+@export var max_float_speed = 600
+@export var float_recharge_speed = 2000
 
 var last_direction = 1
 var time_since_grounded = 0
 var time_since_jump = 0
 var time_since_float = 0
 var jump_state = JumpState.FALLING
-
+var float_left = max_float_time
 
 @onready var main_sprite = $MainSprite
 @onready var collider: Area2D = $Collider
@@ -38,17 +40,25 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	if jump_state == JumpState.FLOATING:
-		time_since_float += delta
-
+		
 	# Apply gravity.
 	if not is_on_floor():
 		time_since_grounded += delta
 		var gravity = gravity_dict[jump_state]
 		velocity.y += gravity * delta
-	
+		if jump_state == JumpState.FLOATING:
+			print(velocity.y)
+			float_left -= delta
+			if -velocity.y > max_float_speed:
+				velocity.y -= gravity * delta * 2
+		
+		
 	if is_on_floor():
 		time_since_grounded = 0
+		if float_left < max_float_time:
+			float_left = min(float_left + float_recharge_speed*delta, max_float_time)
+			
+	
 	
 	# Jump if jump button pressed.
 	handle_jump()
@@ -58,24 +68,28 @@ func _physics_process(delta: float) -> void:
 
 func handle_jump():
 	var just_jumped = Input.is_action_just_pressed("jump") and time_since_grounded < coyote_time
-	var stop_jump = Input.is_action_just_released('jump')
-	
+	var stop_jump = Input.is_action_just_released("jump")
+	var just_floated = Input.is_action_just_pressed("float") 
+	var stop_float = Input.is_action_just_released("float")
+
 	if just_jumped: 
 		time_since_grounded += 10
 		jump_state = JumpState.JUMPING
 		velocity.y = -1 * (jump_power)
+
 	
-	if stop_jump:
-		jump_state = JumpState.FALLING
+	if just_floated:
+		jump_state = JumpState.FLOATING
 	
 	if jump_state == JumpState.JUMPING:
-		if velocity.y > -jump_to_float_v: 
-			jump_state = JumpState.FLOATING
+		if stop_jump: 
+			jump_state = JumpState.FALLING
+		elif velocity.y > - jump_to_fall_v: 
+			jump_state = JumpState.FALLING
 
 	if jump_state == JumpState.FLOATING:
-		if time_since_float > max_float_time:
+		if float_left < 0.0:
 			jump_state = JumpState.FALLING
-			time_since_float = 0
 
 
 func on_death(message: String) -> void:
